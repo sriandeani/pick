b0d8f89f59426158a5aa857cb2046400dc85ca3c
Reynanda Putra Pratama <rey@rushowl.sg>
2020-04-09 16:20:19 +0700

    Initial Commit

---
+++ PICK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PICK
+//
+//  Created by Sri Andeani on 09/04/20.
+//  Copyright © 2020 Sri Andeani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
